a47fecdb236ffcc2f1f1ead9dee7910ee9781500
juniRanger <raincolor54@gmail.com>
2026-03-06 12:42:47 -0600

    removi node modules y database

--- dist/config/db.d.ts
+++
@@ -1,3 +0,0 @@
-import sqlite3 from "sqlite3";
-export declare const initDB: () => Promise<import("sqlite").Database<sqlite3.Database, sqlite3.Statement>>;
-//# sourceMappingURL=db.d.ts.map
--- dist/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get("/", (req, res) => {
-    res.send("Price Sentinel running");
-});
-exports.default = router;
-//# sourceMappingURL=product.routes.js.map
